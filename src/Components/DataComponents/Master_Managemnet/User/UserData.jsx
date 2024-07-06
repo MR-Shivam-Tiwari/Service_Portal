c6f168bc42e1843bc357f289255be0eb6ee9a119
Shivam Tiwari <114647321+MR-Shivam-Tiwari@users.noreply.github.com>
2024-07-06 18:22:49 +0530

    sd

--- src/Components/DataComponents/Master_Managemnet/User/UserData.jsx
+++ src/Components/DataComponents/Master_Managemnet/User/UserData.jsx
@@ -128,7 +128,7 @@ const UserData = () => {
                     </div>
                   </th>
                   <td className="p-4 align-middle whitespace-nowrap">{user.employeeid}</td>
-                  <td className="p-4 align-middle whitespace-nowrap">{user.firstname}</td>
+                  <td className="p-4 align-middle whitespace-nowrap">{user.firstname}</td> 
                   <td className="p-4 align-middle whitespace-nowrap">{user.lastname}</td>
                   <td className="p-4 align-middle whitespace-nowrap">{user.email}</td>
                   <td className="p-4 align-middle whitespace-nowrap">{user.mobilenumber}</td>
